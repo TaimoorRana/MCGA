ae9362ef409ea687b8d56b45768e10ed2448693f
taimoorrana <taimoorrana1@gmail.com>
2017-02-11 17:38:11 -0500

    Added FB,FG,GA,GN and MT Building markers with GroundOverlay
    MCGA-8

--- app/src/main/java/com/concordia/mcga/models/Campus.java
+++ app/src/main/java/com/concordia/mcga/models/Campus.java
@@ -79,6 +79,62 @@ public class Campus extends POI {
                         new LatLng(45.495007, -73.578824),
                         new LatLng(45.495166, -73.579171),
                         new LatLng(45.495222, -73.579114)));
+        // FB
+        MarkerOptions fbMarkerOptions = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_fb_building));
+        buildings.add(new Building(new LatLng(45.494635, -73.577604), "Faubourg", "FB", fbMarkerOptions)
+                .addEdgeCoordinate(new LatLng(45.494695, -73.578040),
+                        new LatLng(45.494911, -73.577785),
+                        new LatLng(45.494654, -73.577219),
+                        new LatLng(45.494397, -73.577521)));
+
+        // FG
+        MarkerOptions fgMarkerOptions = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_fg_building));
+        buildings.add(new Building(new LatLng(45.494170, -73.578351), "Faubourg Ste-Catherine", "FG", fgMarkerOptions)
+                .addEdgeCoordinate(new LatLng(45.493822, -73.579067),
+                        new LatLng(45.494695, -73.578039),
+                        new LatLng(45.494452, -73.577617),
+                        new LatLng(45.494388, -73.577693),
+                        new LatLng(45.494428, -73.577761),
+                        new LatLng(45.494392, -73.577804),
+                        new LatLng(45.494372, -73.577769),
+                        new LatLng(45.494187, -73.577987),
+                        new LatLng(45.493626, -73.578728)));
+        // GA
+        MarkerOptions gaMarkerOptions = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_ga_building));
+        buildings.add(new Building(new LatLng(45.494096, -73.577944), "Grey Nuns Annex", "GA", gaMarkerOptions)
+                .addEdgeCoordinate(new LatLng(45.493851, -73.578354),
+                        new LatLng(45.494134, -73.578012),
+                        new LatLng(45.494119, -73.577986),
+                        new LatLng(45.494345, -73.577737),
+                        new LatLng(45.494285, -73.577622),
+                        new LatLng(45.494056, -73.577871),
+                        new LatLng(45.494076, -73.577908),
+                        new LatLng(45.493790, -73.578253)));
+
+        //GN
+        MarkerOptions gnMarkerOptions = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_gn_building));
+        buildings.add(new Building(new LatLng(45.493687, -73.576280), "Grey Nuns", "GN", gnMarkerOptions)
+                .addEdgeCoordinate(new LatLng(45.493908, -73.577904),
+                        new LatLng(45.494239, -73.577543),
+                        new LatLng(45.494268, -73.577602),
+                        new LatLng(45.494379, -73.577488),
+                        new LatLng(45.494397, -73.577519),
+                        new LatLng(45.494683, -73.577166),
+                        new LatLng(45.493833, -73.575183),
+                        new LatLng(45.492341, -73.576632),
+                        new LatLng(45.492969, -73.577728),
+                        new LatLng(45.493507, -73.577160)));
+
+        //MT
+        MarkerOptions mtMarkerOptions = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_mt_building));
+        buildings.add(new Building(new LatLng(45.494556, -73.576056), "Montefiore", "MT", mtMarkerOptions)
+                .addEdgeCoordinate(new LatLng(45.494421, -73.576380),
+                        new LatLng(45.494711, -73.576123),
+                        new LatLng(45.494679, -73.576009),
+                        new LatLng(45.494823, -73.575883),
+                        new LatLng(45.494752, -73.575719),
+                        new LatLng(45.494448, -73.575984),
+                        new LatLng(45.494298, -73.576131)));
 
 
     }
