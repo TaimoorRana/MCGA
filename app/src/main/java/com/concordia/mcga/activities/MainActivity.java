1d43d820fd8792568f6da666eda77cb5d8593fa2
Mark Snidal <mark.snidal@gmail.com>
2017-02-11 23:29:12 -0500

    Drawer menu structure
    Fragmentization of main activity, refactoring layout to separate files.
    Added side drawer

---
+++ app/src/main/java/com/concordia/mcga/activities/MainActivity.java
@@ -0,0 +1,99 @@
+package com.concordia.mcga.activities;
+
+    import android.support.design.widget.NavigationView;
+    import android.support.v4.widget.DrawerLayout;
+    import android.support.v7.app.ActionBarDrawerToggle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.os.Bundle;
+    import android.support.v7.widget.Toolbar;
+    import android.view.Menu;
+    import android.view.MenuItem;
+    import android.view.View;
+    import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()){
+                    case R.id.navigation:
+                        NavigationFragment fragment = new NavigationFragment();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.frame, fragment);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.shuttle_schedule:
+                        Toast.makeText(getApplicationContext(),"Shuttle Schedule",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.settings:
+                        Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.student_spots:
+                        Toast.makeText(getApplicationContext(),"Student Spots",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.about:
+                        Toast.makeText(getApplicationContext(),"About",Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Somethings Wrong",Toast.LENGTH_SHORT).show();
+                        return false;
+                }
+            }
+        });
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.open_drawer, R.string.close_drawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
