ac598553e1a8dad431368a7febb0d59fe7cdecbf
taimoorrana <taimoorrana1@gmail.com>
2017-02-12 13:17:32 -0500

    Added a style to our Google Maps in order to remove unwanted POI provided by Google Maps by default
    MCGA-102

--- app/src/main/java/com/concordia/mcga/activities/MapsActivity.java
+++ app/src/main/java/com/concordia/mcga/activities/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.concordia.mcga.activities;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +13,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -47,6 +50,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e("Google Map Style", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("Google Map Style", "Can't find style. Error: ", e);
+        }
         googleMap.addMarker(LOYOLA_MARKER);
         googleMap.addMarker(SGW_MARKER);
 
