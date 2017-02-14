04a702f2e908eebbfa68d719dc18253cb54a41e0
Arek Mano <manoukian.arek@gmail.com>
2017-02-13 22:20:57 -0500

    Fixed unit test for the IndoorMapFactory.

--- app/src/test/java/com/concordia/mcga/factories/IndoorMapFactoryTest.java
+++ app/src/test/java/com/concordia/mcga/factories/IndoorMapFactoryTest.java
@@ -1,18 +1,39 @@
 package com.concordia.mcga.factories;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import android.content.Context;
+import android.content.res.Resources;
+import com.concordia.mcga.models.IndoorMap;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import junit.framework.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Created by arekm on 2017-02-13.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class IndoorMapFactoryTest {
     @Test
-    public void testFactory() throws Exception {
-        File file = new File("res/raw/hall4.txt");
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        System.out.println(reader.readLine());
+    public void testGetHall4thFloor() throws Exception {
+        // Test Data
+        Context context = Mockito.mock(Context.class);
+
+        // Mock
+        Resources mockResources = Mockito.mock(Resources.class);
+        Mockito.when(context.getResources()).thenReturn(mockResources);
+        Mockito.when(mockResources.openRawResource(Mockito.anyInt())).thenReturn(new ByteArrayInputStream("3,4\n4,5".getBytes(
+            StandardCharsets.UTF_8)));
+
+        // Execute
+        IndoorMap result = IndoorMapFactory.getHall4thFloor(context);
+
+        // Verify
+        Mockito.verify(mockResources).openRawResource(Mockito.anyInt());
+        Mockito.verify(context).getResources();
+        Assert.assertNotNull(result.getMap().getTile(3, 4));
+        Assert.assertNotNull(result.getMap().getTile(4, 5));
     }
 }
