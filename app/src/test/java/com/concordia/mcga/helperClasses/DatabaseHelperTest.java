a1a67333a79517f182ef2aef60a693a004bdf354
Arek Mano <manoukian.arek@gmail.com>
2017-03-04 14:40:54 -0500

    ; troubles

--- app/src/test/java/com/concordia/mcga/helperClasses/DatabaseHelperTest.java
+++ app/src/test/java/com/concordia/mcga/helperClasses/DatabaseHelperTest.java
@@ -3,14 +3,12 @@ package com.concordia.mcga.helperClasses;
 import com.concordia.mcga.exceptions.MCGADatabaseException;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 
 public class DatabaseHelperTest {
 
     @Test(expected = MCGADatabaseException.class)
     public void getInstanceWithoutContext() throws Exception {
-        DatabaseHelper.getInstance()
+        DatabaseHelper.getInstance();
     }
 
 }
