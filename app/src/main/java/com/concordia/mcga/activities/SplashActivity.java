d0854a14b76addf21e7ac5795dd204bdf7328241
taimoorrana <taimoorrana1@gmail.com>
2017-03-09 19:56:25 -0500

    MCGA-115 Layout not needed

--- app/src/main/java/com/concordia/mcga/activities/SplashActivity.java
+++ app/src/main/java/com/concordia/mcga/activities/SplashActivity.java
@@ -15,7 +15,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
