e789f37e412bfec6a4e3704f5a188c63f45ab7a7
Adrianna Diaz <adrianna.dev@gmail.com>
2017-03-22 14:20:55 -0400

    Removed unused imports from SideMenuTest

--- app/src/androidTest/java/com/concordia/mcga/activities/SideMenuTest.java
+++ app/src/androidTest/java/com/concordia/mcga/activities/SideMenuTest.java
@@ -5,13 +5,7 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
