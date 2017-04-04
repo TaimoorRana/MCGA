7dacddce80ebb0ac0649b2f34366f1b58ac77aed
Mark Snidal <mark.snidal@gmail.com>
2017-04-03 20:13:31 -0400

    MCGA-26: Updated adapter with distance formatting and layout population

--- app/src/main/java/com/concordia/mcga/adapters/StudentSpotAdapter.java
+++ app/src/main/java/com/concordia/mcga/adapters/StudentSpotAdapter.java
@@ -5,25 +5,65 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.concordia.mcga.activities.R;
 import com.concordia.mcga.models.StudentSpot;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.text.DecimalFormat;
+import java.util.List;
 
 public class StudentSpotAdapter extends ArrayAdapter<StudentSpot> {
     private final Context context;
+    private final List<StudentSpot> spots;
+    private final DecimalFormat df = new DecimalFormat(".##");
+    private LatLng currentCoordinates;
 
-    public StudentSpotAdapter(Context context, int resource) {
-        super(context, resource);
+    public StudentSpotAdapter(Context context, List<StudentSpot> spots) {
+        super(context, R.layout.student_spot_row, spots);
         this.context = context;
+        this.spots = spots;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
         View spotRow = inflater.inflate(R.layout.student_spot_row, parent, false);
 
+        TextView name = (TextView) spotRow.findViewById(R.id.spotTitle);
+        TextView address = (TextView) spotRow.findViewById(R.id.spotAddress);
+        TextView distance = (TextView) spotRow.findViewById(R.id.spotDistance);
+        TextView description = (TextView) spotRow.findViewById(R.id.spotDescription);
+        ImageView image = (ImageView) spotRow.findViewById(R.id.spotImage);
+        RatingBar rating = (RatingBar) spotRow.findViewById(R.id.spotRating);
 
+        name.setText(getItem(position).getName());
+        address.setText(getItem(position).getAddress());
+        distance.setText(getFormattedDistance(currentCoordinates,
+                getItem(position).getMapCoordinates()));
+        description.setText(getItem(position).getDescription());
+        image.setImageResource(getItem(position).getResId());
+        rating.setRating(getItem(position).getRating().getRatingVal());
 
         return spotRow;
+    }
+
+    private String getFormattedDistance(LatLng start, LatLng end) {
+        final int radius = 6371;
+
+        double lat  = Math.toRadians(end.latitude - start.latitude);
+        double lon = Math.toRadians(end.longitude - end.longitude);
+
+        double a = Math.sin(lat / 2) * Math.sin(lat / 2)
+                + Math.cos(Math.toRadians(start.latitude)) * Math.cos(Math.toRadians(end.latitude))
+                * Math.sin(lon / 2) * Math.sin(lon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = radius * c * 1000;
+
+        return df.format(distance) + " m";
     }
 }
--- app/src/main/java/com/concordia/mcga/models/StudentSpot.java
+++ app/src/main/java/com/concordia/mcga/models/StudentSpot.java
@@ -5,30 +5,36 @@ import com.google.android.gms.maps.model.LatLng;
 public class StudentSpot extends POI {
     private RATING rating;
     private String description;
+    private String address;
+
+    private int resId;
 
     public enum RATING {
-        NO_STARS    (0.0),
-        ONE_STAR    (1.0),
-        TWO_STARS   (2.0),
-        THREE_STARS (3.0),
-        FOUR_STARS  (4.0),
-        FIVE_STARS  (5.0);
+        NO_STARS    (0.0f),
+        ONE_STAR    (1.0f),
+        TWO_STARS   (2.0f),
+        THREE_STARS (3.0f),
+        FOUR_STARS  (4.0f),
+        FIVE_STARS  (5.0f);
 
-        private double ratingVal;
+        private float ratingVal;
 
-        RATING(double ratingVal) {
+        RATING(float ratingVal) {
             this.ratingVal = ratingVal;
         }
 
-        public double getRatingVal() {
+        public float getRatingVal() {
             return this.ratingVal;
         }
     }
 
-    public StudentSpot(LatLng mapCoordinates, String name, RATING rating, String description) {
+    public StudentSpot(LatLng mapCoordinates, String name, RATING rating, String description,
+                       String address, int resId) {
         super(mapCoordinates, name);
         this.rating = rating;
         this.description = description;
+        this.address = address;
+        this.resId = resId;
     }
 
     public RATING getRating() {
@@ -45,5 +51,21 @@ public class StudentSpot extends POI {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getResId() {
+        return resId;
+    }
+
+    public void setResId(int resId) {
+        this.resId = resId;
     }
 }
