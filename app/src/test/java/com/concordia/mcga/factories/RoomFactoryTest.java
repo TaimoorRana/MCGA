9c37fc9b34355d1719466a0635a60386890b2628
Arek Mano <manoukian.arek@gmail.com>
2017-04-04 19:29:11 -0400

    fixed another test. MCGA-94

--- app/src/test/java/com/concordia/mcga/factories/RoomFactoryTest.java
+++ app/src/test/java/com/concordia/mcga/factories/RoomFactoryTest.java
@@ -1,21 +1,22 @@
 package com.concordia.mcga.factories;
 
+import static com.concordia.mcga.factories.RoomFactory.ROOM_ICON_COLUMN_INDEX;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 import android.database.Cursor;
-import com.concordia.mcga.activities.BuildConfig;
 import com.concordia.mcga.models.Room;
+import com.concordia.mcga.models.Room.RoomIcon;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, shadows = ShadowBitmapDescriptorFactory.class)
+@RunWith(MockitoJUnitRunner.class)
+@PrepareForTest(RoomFactory.class)
 public class RoomFactoryTest {
     private Gson gson = new Gson();
 
@@ -29,10 +30,11 @@ public class RoomFactoryTest {
         Mockito.when(res.getString(RoomFactory.CENTER_COORDINATE_COLUMN_INDEX)).thenReturn(gson.toJson(latLng));
         Mockito.when(res.getString(RoomFactory.NAME_COLUMN_INDEX)).thenReturn(name);
         Mockito.when(res.getString(RoomFactory.ROOM_NUMBER_COLUMN_INDEX)).thenReturn(roomNumber);
+        Mockito.when(res.getString(ROOM_ICON_COLUMN_INDEX)).thenReturn(RoomIcon.NONE.toString());
 
         Room result = RoomFactory.createRoom(res);
 
-        assertTrue(result instanceof Room);
+        assertNotNull(result);
         assertEquals(latLng, result.getMapCoordinates());
         assertEquals(name, result.getName());
         assertEquals(roomNumber, result.getRoomNumber());
