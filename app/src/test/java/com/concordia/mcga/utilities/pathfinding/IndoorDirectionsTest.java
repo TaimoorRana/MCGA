f51cb10d0672aa077d115a7eb0bebbc13aa2ff23
Samuel-Campbell <samuel.pcampbell@gmail.com>
2017-04-05 01:05:04 -0400

    Added Test Class for IndoorDirections. Still empty methods just saving work done till tomorrow

---
+++ app/src/test/java/com/concordia/mcga/utilities/pathfinding/IndoorDirectionsTest.java
@@ -0,0 +1,16 @@
+package com.concordia.mcga.utilities.pathfinding;
+
+
+import com.concordia.mcga.activities.BuildConfig;
+
+import org.junit.runner.RunWith;
+
+
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class IndoorDirectionsTest {
+}
